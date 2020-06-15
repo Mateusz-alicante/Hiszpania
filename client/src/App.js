fe1784326d316e4dac52e14a6cb334b36ff88d04
Mateusz <mateusz.alicante@gmail.com>
2020-06-15 13:31:13 +0200

    17

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    message: "No message yet"
+  }
+
+  fetchData = async () => {
+    const message = await fetch('/api/test')
+    const JsonMessage = await message.json()
+    this.setState({ message: JsonMessage.message })
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  render() {
+    return(
+      <div>
+        <h1> React App </h1>
+        <p> {this.state.message} </p>
+      </div>
+    )
+  }
 }
 
 export default App;
