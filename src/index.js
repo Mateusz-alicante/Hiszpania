733ae56fdeeda464d59b8c8db32768256c9c4eec
Mateusz <mateusz.alicante@gmail.com>
2020-06-15 08:11:02 +0200

    7

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '/../client/public/')));
-
 app.get('/api/test', (req, res) => {
     res.json({message: "Hello World from node"})
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/../client/public/index.html'));
-});
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname+'/../client/public')));
+    const path = require('path');
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+    });
+}
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
