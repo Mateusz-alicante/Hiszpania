02511c3f608d9e15339a2e5ea4938c2ed1cebeb9
Mateusz <mateusz.alicante@gmail.com>
2020-06-15 07:46:38 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+console.log("Hello world")
+
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '/../build/public')));
+
+app.get('/api/test', (req, res) => {
+    res.json({message: "Hello World from node"})
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../client/public/index.html'));
+  });
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
