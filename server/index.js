6b6be172c5458d5ecd824778b884e568292b1b93
Mateusz <mateusz.alicante@gmail.com>
2020-06-15 14:18:00 +0200

    18

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 app.get('/api/test', (req, res) => {
     res.json({message: "Hello World from node"})
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/../client/build/index.html'));
   });
 
 
