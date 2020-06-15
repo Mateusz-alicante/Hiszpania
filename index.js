a6a33d57e5ed1ba4b033cc99d23b062026310cfa
Mateusz <mateusz.alicante@gmail.com>
2020-06-15 08:17:03 +0200

    10

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/api/test', (req, res) => {
     res.json({message: "Hello World from node"})
 })
 
-
-if (process.env.NODE_ENV === 'production') {
-    const path = require('path');
-    app.use(express.static(path.join(__dirname+'/../client/build')));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/../client/build/index.html'));
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
